45410fab310b23e3b661c9526ef205fa1b2f35bb
Tom <tom@temper.systems>
2023-12-14 04:48:17 -0700

    Process form posts
    Signed-off-by: Tom <tom@temper.systems>

--- form-validation/server-csharp/Program.cs
+++ form-validation/server-csharp/Program.cs
@@ -1,43 +1,64 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace ServerCSharp
 {
+    class RawForm
+    {
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+    }
+
+    record Result(string Message);
+
     static class Program
     {
         static void Main(string[] args)
         {
             var app = BuildApp(args);
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapPost(
+                "/",
+                (RawForm form) =>
+                {
+                    Console.WriteLine(form);
+                    return new Result(Message: "Form processed in C#");
+                }
+            );
 
             app.Run();
         }
 
         static WebApplication BuildApp(string[] args)
         {
+            var version = "v0";
             var builder = WebApplication.CreateBuilder(args);
+            // Configure Swagger service.
             builder.Services.AddEndpointsApiExplorer();
             builder
                 .Services
                 .AddSwaggerGen(c =>
                 {
                     c.SwaggerDoc(
-                        "v0",
+                        version,
                         new OpenApiInfo
                         {
                             Title = "Temper Validation Demo Server",
                             Description = "Demo Temper-Built Validation Logic in C# Server",
-                            Version = "v0"
+                            Version = version
                         }
                     );
                 });
+            // Build app.
             var app = builder.Build();
+            // Use Swagger.
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v0/swagger.json", "Temper Validation Demo");
+                c.SwaggerEndpoint($"/swagger/{version}/swagger.json", "Temper Validation Demo");
             });
+            // Done.
             return app;
         }
     }
