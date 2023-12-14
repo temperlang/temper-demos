a7267aca8c91f6451e6aa048f902f1519af9ac1b
Tom <tom@temper.systems>
2023-12-14 04:29:47 -0700

    Add swagger
    Signed-off-by: Tom <tom@temper.systems>

--- form-validation/server-csharp/Program.cs
+++ form-validation/server-csharp/Program.cs
@@ -1,6 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
 
-app.MapGet("/", () => "Hello World!");
+namespace ServerCSharp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = BuildApp(args);
 
-app.Run();
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+
+        static WebApplication BuildApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddEndpointsApiExplorer();
+            builder
+                .Services
+                .AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc(
+                        "v0",
+                        new OpenApiInfo
+                        {
+                            Title = "Temper Validation Demo Server",
+                            Description = "Demo Temper-Built Validation Logic in C# Server",
+                            Version = "v0"
+                        }
+                    );
+                });
+            var app = builder.Build();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v0/swagger.json", "Temper Validation Demo");
+            });
+            return app;
+        }
+    }
+}
